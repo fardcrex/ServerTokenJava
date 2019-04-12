f8914550abf62802265314761d32f2fbef3de264
Jair Conislla <1632874@utp.edu.pe>
2019-04-11 20:45:47 -0500

    feat Autentificacion
    Se agrega funcionalidad de respuestas

--- src/main/java/com/lacoraza/javajuniorappbackend/rutasRESTful/Autentificacion_Server.java
+++ src/main/java/com/lacoraza/javajuniorappbackend/rutasRESTful/Autentificacion_Server.java
@@ -1,6 +1,7 @@
 package com.lacoraza.javajuniorappbackend.rutasRESTful;
 
-import com.lacoraza.javajuniorappbackend.contralador.Autentificacion_TOKEN;
+import com.lacoraza.javajuniorappbackend.LogicaDeNegocios.Autentificacion_TOKEN;
+import com.lacoraza.javajuniorappbackend.modelos.Usuario;
 
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -9,6 +10,7 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
+import java.util.HashMap;
 import java.util.Map;
 
 @Path("autentificacion")
@@ -20,27 +22,32 @@ public class Autentificacion_Server {
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     public Response obtenerJWT(Map<String,String> credenciales,@Context UriInfo uriInfo) {
+        try {
+            Map<String, Object> respuesta = autentificacion_token.verificarCredenciales(credenciales);
 
-        Map<String, Object> respuesta = autentificacion_token.verificarCredenciales(credenciales);
+            switch ((int)respuesta.get("status")){
+                case 1: return Response.status(Response.Status.PARTIAL_CONTENT).entity("Falta los campos requeridos").build();
 
+                case 2: return Response.status(Response.Status.PARTIAL_CONTENT).entity("Hay campos vacíos").build();
 
-        if (((boolean)respuesta.get("status")) == true) {
+                case 3: return Response.status(Response.Status.OK).entity("404 : usuario inexistente").build();
 
-            Map<String, String>  tokenjson = autentificacion_token.crearTOKEN(respuesta);
-            return Response.status(Response.Status.ACCEPTED).entity(tokenjson).build();
+                case 4: Map<String, Object>  tokenjson = new HashMap<>();
+                        Map<String, Object>  usuario = new HashMap<>();
 
-        } else {
+                        usuario.put("id",     ((Usuario) respuesta.get("Usuario")).getId());
+                        usuario.put("Correo", ((Usuario)respuesta.get("Usuario")).getCorreo());
+                        usuario.put("Nombre", ((Usuario)respuesta.get("Usuario")).getNombre());
 
+                        tokenjson.put("Usuario",usuario);
+                        tokenjson.put("Token"  ,autentificacion_token.crearTOKEN(respuesta));
+                        return Response.status(Response.Status.ACCEPTED).entity(tokenjson).build();
+
+                default:return Response.status(Response.Status.UNAUTHORIZED).entity(credenciales).build();
+            }
+        }catch (Exception e){
             return Response.status(Response.Status.UNAUTHORIZED).entity(credenciales).build();
         }
     }
-
-
-
-
-
-
-
-
 
 }
